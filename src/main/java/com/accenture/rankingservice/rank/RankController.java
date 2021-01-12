c1833109a3b611dd86a861a5a217cc405d7ae1f9
sinablatt <sina.blattmann@accenture.com>
2021-01-12 15:58:35 +0100

    added ka

--- src/main/java/com/accenture/rankingservice/rank/RankController.java
+++ src/main/java/com/accenture/rankingservice/rank/RankController.java
@@ -1,5 +1,7 @@
 package com.accenture.rankingservice.rank;
 
+import com.accenture.rankingservice.Category.Category;
+import com.accenture.rankingservice.Category.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,29 +12,36 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/rank")
 public class RankController {
 
-    @Autowired
-    RankService rankService;
+  @Autowired
+  RankService rankService;
 
-    @GetMapping({"/{id}", "/{id}/"})
-    public ResponseEntity<Optional<Rank>> findById(@PathVariable("id") String id) {
-        return new ResponseEntity<>(rankService.findById(id), HttpStatus.OK);
-    }
+  @Autowired
+  CategoryService categoryService;
 
-    @GetMapping({"/", ""})
-    public ResponseEntity<List<Rank>> findAll(){
-        return new ResponseEntity<>(rankService.findAll(), HttpStatus.OK);
-    }
+  @GetMapping({"/{id}", "/{id}/"})
+  public ResponseEntity<Optional<Rank>> findById(@PathVariable("id") String id) {
+    return new ResponseEntity<>(rankService.findById(id), HttpStatus.OK);
+  }
 
-    @GetMapping({"/category/{categoryName}", "/category/{categoryName}/"})
-    public ResponseEntity<List<Rank>> findByCategory(@PathVariable("categoryName") String categoryName){
-        return new ResponseEntity<>(rankService.findByCategory(categoryName), HttpStatus.OK);
-    }
+  @GetMapping({"/", ""})
+  public ResponseEntity<List<Rank>> findAll() {
+    return new ResponseEntity<>(rankService.findAll(), HttpStatus.OK);
+  }
 
-    @PostMapping("/create")
-    public ResponseEntity<Rank> save(@RequestBody Rank rank) {
-        return new ResponseEntity<>(rankService.save(rank), HttpStatus.CREATED);
-    }
+  @GetMapping({"/category/{categoryName}", "/category/{categoryName}/"})
+  public ResponseEntity<List<Rank>> findByCategory(@PathVariable("categoryName") String categoryName) {
+    return new ResponseEntity<>(rankService.findByCategory(categoryName), HttpStatus.OK);
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<Rank> save(@RequestBody Rank rank) {
+    String categoryName = rank.getCategory().getName();
+    Category category = categoryService.findByName(categoryName);
+    rank.setCategory(category);
+    return new ResponseEntity<>(rankService.save(rank), HttpStatus.CREATED);
+  }
 }
